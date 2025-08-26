ac16026b5260be0e7c5ecfeb6c9def2d69ceb8bb
rae-afghani <razankafghani@gmail.com>
2025-08-26 17:48:31 -0500

    created spacing components (XS, SM, MD, LG, and XL)

---
+++ DopamineOS/components/Spacers.tsx
@@ -0,0 +1,62 @@
+import type { PropsWithChildren, ReactElement } from 'react';
+import { StyleSheet } from 'react-native';
+
+
+import { ThemedView } from '@/components/ThemedView';
+import { useBottomTabOverflow } from '@/components/ui/TabBarBackground';
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+const spacerXS = 8;
+const spacerSM = 12;
+const spacerMD = 16;
+const spacerLG = 24;
+const spacerXL = 32;
+
+export function SpacerXS(){
+  return (
+    <ThemedView style={styles.spacerXS}></ThemedView>
+  );
+}
+export function SpacerSM(){
+  return (
+    <ThemedView style={styles.spacerSM}></ThemedView>
+  );
+}
+export default function SpacerMD(){
+  return (
+    <ThemedView style={styles.spacerMD}></ThemedView>
+  );
+}
+export function SpacerLG(){
+  return (
+    <ThemedView style={styles.spacerLG}></ThemedView>
+  );
+}
+export function SpacerXL(){
+  return (
+    <ThemedView style={styles.spacerXL}></ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  spacerXS: {
+    height: spacerXS,
+    overflow: 'hidden',
+  },
+  spacerSM: {
+    height: spacerSM,
+    overflow: 'hidden',
+  },
+  spacerMD: {
+    height: spacerMD,
+    overflow: 'hidden',
+  },
+  spacerLG: {
+    height: spacerLG,
+    overflow: 'hidden',
+  },
+  spacerXL: {
+    height: spacerXL,
+    overflow: 'hidden',
+  },
+});
